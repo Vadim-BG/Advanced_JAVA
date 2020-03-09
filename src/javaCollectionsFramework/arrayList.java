f35eaefd245aaf54290fda5cea6737028aeb049e
Vadim-BG <infotourbg.vadim@gmail.com>
2020-03-09 15:10:56 +0200

    Динамический массив (ArrayList) - Как устроен?

---
+++ src/javaCollectionsFramework/arrayList.java
@@ -0,0 +1,25 @@
+package javaCollectionsFramework;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class arrayList {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        list.remove(5);
+        System.out.println(list);
+
+
+        //мы проводим много удалений из нашего листа
+
+        list = new LinkedList<>();
+    }
+}

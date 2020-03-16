b69d7bf2932a49322bd8fefbe626d13087ed8339
Vadim-BG <infotourbg.vadim@gmail.com>
2020-03-16 11:46:11 +0200

    HashMap - Как устроен?

---
+++ src/javaCollectionsFramework/hashmap/TestHashMap.java
@@ -0,0 +1,20 @@
+package javaCollectionsFramework.hashmap;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class TestHashMap {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("Bob", 25);
+        map.put("Katy", 15);
+        map.put("Bill", 40);
+
+        Set<String> set = new HashSet<>();
+
+        System.out.println(map);
+    }
+}

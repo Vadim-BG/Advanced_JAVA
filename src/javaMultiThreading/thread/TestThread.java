55a2903c8a46d06d2fb29243ff0fe631362667ee
Vadim-BG <infotourbg.vadim@gmail.com>
2020-03-19 15:02:49 +0200

    Введение в многопоточность в Java

---
+++ src/javaMultiThreading/thread/TestThread.java
@@ -0,0 +1,44 @@
+package javaMultiThreading.thread;
+
+public class TestThread {
+    public static void main(String[] args) throws InterruptedException {
+//        MyThread myThread = new MyThread();
+//        myThread.start();
+//
+//        MyThread myThread2 = new MyThread();
+//        myThread2.start();
+
+        Thread thread = new Thread(new Runner());
+        thread.start();
+
+        System.out.println("Hello from main Thread");
+    }
+}
+
+class Runner implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(i + " : Hello from MyThread");
+        }
+    }
+}
+
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(i + " : Hello from MyThread");
+        }
+    }
+}

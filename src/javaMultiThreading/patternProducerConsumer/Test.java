4225a1e8db1092538012fb09ce1789caae9c158f
Vadim-BG <infotourbg.vadim@gmail.com>
2020-03-23 13:23:04 +0200

    Паттерн producer - consumer (II часть)

--- src/javaMultiThreading/patternProducerConsumer/Test.java
+++ src/javaMultiThreading/patternProducerConsumer/Test.java
@@ -1,4 +1,4 @@
-package javaMultiThreading.patternProduscerConsumer;
+package javaMultiThreading.patternProducerConsumer;
 
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;

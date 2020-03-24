226f725c19624ac77e346fc5ee4798b82889725c
Vadim-BG <infotourbg.vadim@gmail.com>
2020-03-24 15:32:18 +0200

    Класс CountDownLatch вариант

--- src/javaMultiThreading/count_DownLatch/Test.java
+++ src/javaMultiThreading/count_DownLatch/Test.java
@@ -13,20 +13,24 @@ public class Test {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
         for (int i = 0; i < 3; i++)
-            executorService.submit(new Processor(countDownLatch));
+            executorService.submit(new Processor(i, countDownLatch));
 
         executorService.shutdown();
 
-        countDownLatch.await();
-        System.out.println("Latch has been opened, main thread is proceeding!");
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(1000);
+            countDownLatch.countDown();
+        }
+
     }
 }
 
 class Processor implements Runnable {
-
+    private int id;
     private CountDownLatch countDownLatch;
 
-    public Processor(CountDownLatch countDownLatch) {
+    public Processor(int id, CountDownLatch countDownLatch) {
+        this.id = id;
         this.countDownLatch = countDownLatch;
     }
 
@@ -38,6 +42,12 @@ class Processor implements Runnable {
             e.printStackTrace();
         }
 
-        countDownLatch.countDown();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Thread with id " + id + " proceeded.");
     }
 }

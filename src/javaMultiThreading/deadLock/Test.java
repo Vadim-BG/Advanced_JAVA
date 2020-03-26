2c89ea58d3696989398969af9b1bb4c6e3ba6dae
Vadim-BG <infotourbg.vadim@gmail.com>
2020-03-26 12:43:12 +0200

    Взаимная блокировка (DeadLock) with Synchronised

--- src/javaMultiThreading/deadLock/Test.java
+++ src/javaMultiThreading/deadLock/Test.java
@@ -1,8 +1,6 @@
 package javaMultiThreading.deadLock;
 
 import java.util.Random;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
     public static void main(String[] args) throws InterruptedException {
@@ -35,59 +33,25 @@ class Runner {
     private Account account1 = new Account();
     private Account account2 = new Account();
 
-    private Lock lock1 = new ReentrantLock();
-    private Lock lock2 = new ReentrantLock();
-
-    private void takeLocks(Lock lock1, Lock lock2) {
-        boolean firstLockTaken = false;
-        boolean secondLockTaken = false;
-        while (true) {
-            try {
-                firstLockTaken = lock1.tryLock();
-                secondLockTaken = lock2.tryLock();
-            } finally {
-                if (firstLockTaken && secondLockTaken) {
-                    return;
-                }
-                if (firstLockTaken) {
-                    lock1.unlock();
-                }
-                if (secondLockTaken) {
-                    lock2.unlock();
-                }
-            }
-
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     public void firstThread() {
         Random random = new Random();
 
         for (int i = 0; i < 10000; i++) {
-
-            takeLocks(lock1, lock2);
-            try {
-                Account.transfer(account1, account2, random.nextInt(100));
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
+            synchronized (account1){
+                synchronized (account2){
+                    Account.transfer(account1, account2, random.nextInt(100));
+                }
             }
         }
     }
     public void secondThread() {
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
-            takeLocks(lock2, lock1);
-            try {
-                Account.transfer(account2, account1, random.nextInt(100));
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
+            synchronized (account2) {
+                synchronized (account1) {
+                    Account.transfer(account2, account1, random.nextInt(100));
+                }
             }
         }
     }
